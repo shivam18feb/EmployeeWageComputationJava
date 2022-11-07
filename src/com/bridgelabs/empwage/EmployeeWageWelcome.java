7ad4ecea318f5b9baeee27f4cf9ec0ec7a1fec89
unknown <shivam18feb@gmail.com>
2022-11-07 15:18:02 +0530

    Employee_wage_Welcome

---
+++ src/com/bridgelabs/empwage/EmployeeWageWelcome.java
@@ -0,0 +1,10 @@
+package com.bridgelabs.empwage;
+
+public class EmployeeWageWelcome {
+	public static void main (String args[]) {
+		
+		System.out.println(" Welcome to Employee Wage Computation Program");
+		
+	}
+
+}

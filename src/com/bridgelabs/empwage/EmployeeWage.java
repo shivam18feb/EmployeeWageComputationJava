227732e9d0a442c56775368cb15c40dbb6a2748e
unknown <shivam18feb@gmail.com>
2022-11-07 17:42:10 +0530

    EmpPresentAbsentCheck

---
+++ src/com/bridgelabs/empwage/EmployeeWage.java
@@ -0,0 +1,13 @@
+package com.bridgelabs.empwage;
+
+public class EmployeeWage {
+	int IS_PRESENT = 1;
+	public void checkIsPresent(double empCheck) {
+		if (empCheck == IS_PRESENT)
+			System.out.println("Its Present");
+		else
+			System.out.println("Its Present");
+			
+	}
+
+}
--- src/com/bridgelabs/empwage/EmployeeWageWelcome.java
+++ src/com/bridgelabs/empwage/EmployeeWageWelcome.java
@@ -3,7 +3,10 @@ package com.bridgelabs.empwage;
 public class EmployeeWageWelcome {
 	public static void main (String args[]) {
 		
-		System.out.println(" Welcome to Employee Wage Computation Program");
+		System.out.println(" Welcome to Employee Wage Computation Program"); // Master Branch  
+		EmployeeWage emp = new EmployeeWage();
+		double empCheck = Math.floor(Math.random()*10) % 2;
+		emp.checkIsPresent(empCheck);
 		
 	}
 

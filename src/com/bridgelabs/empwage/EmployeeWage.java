6e449a64d1f357318d7c8f8d7cfc4854a5bab213
unknown <shivam18feb@gmail.com>
2022-11-07 19:28:43 +0530

    EmpWageCondtion

--- src/com/bridgelabs/empwage/EmployeeWage.java
+++ src/com/bridgelabs/empwage/EmployeeWage.java
@@ -2,43 +2,55 @@ package com.bridgelabs.empwage;
 
 public class EmployeeWage {
 	final int IS_PRESENT = 1;
-	int WAGE_PER_HRS = 20;
-	int FULL_DAY_HRS = 16;
 	final int FULL_TIME = 1;
-	final int WORKING_DAYS = 20;
+	int WAGE_PER_HRS;
+	int FULL_DAY_HRS = 16;
+	int TOTAL_ATTENDANCE = 0;
+	int TOTAL_HRS = 0;
+	int WORKING_DAYS;
 	
-	public boolean checkIsPresent(double empCheck) {
-		if(empCheck == IS_PRESENT) {
-			return true;
-		}
-		else {
-			return false;
-		}
-			
-	}
-	 public int calcDailyEmpWage(int empType) {
-		 int dailyEmpWage = 0;
-		 switch (empType) {
-		 	case FULL_TIME :
-		 		dailyEmpWage =WAGE_PER_HRS * FULL_DAY_HRS ;
-			 break;
-		 default :
-			 dailyEmpWage = dailyEmpWage/2;
-			}
-		 return dailyEmpWage;
-	 }
-		 public int calcMonthlyWage() {
-			 int monthlyWage = 0;
-			 int dailyWage;
-			 for(int day=1; day<=WORKING_DAYS;day++) {
-				 int empCheck = (int)(Math.random()*10)%2;
-				 if(checkIsPresent(empCheck)) {
-					 int empType =(int)(Math.random()*10)%2;
-					 dailyWage = calcDailyEmpWage(empType);
-					 monthlyWage += dailyWage;
-				 }
-			 }
-			 return monthlyWage;
-	 }
-
+	  public EmployeeWage(int workingDays , int wagePerHr){ 
+	         this.WORKING_DAYS = workingDays; 
+	         this.WAGE_PER_HRS = wagePerHr; 
+	     } 
+	     public boolean checkIsPresent(int empCheck){ 
+	         if(empCheck == IS_PRESENT){  
+	             TOTAL_ATTENDANCE++; 
+	             return true; 
+	         } 
+	         else{  
+	             return false; 
+	         } 
+	     } 
+	     public int calcDailyEmpWage(int empType){ 
+	  
+	         int dailyEmpWage = 0; 
+	         switch(empType){ 
+	             case  FULL_TIME : 
+	                 TOTAL_HRS += FULL_DAY_HRS; 
+	                 dailyEmpWage =  WAGE_PER_HRS * FULL_DAY_HRS; 
+	             break; 
+	             default : 
+	                 TOTAL_HRS += FULL_DAY_HRS / 2;  
+	                 dailyEmpWage = dailyEmpWage / 2; 
+	         } 
+	         return dailyEmpWage; 
+	     } 
+	     public int calcMonthlyWage(){ 
+	  
+	         int monthlyWage = 0; 
+	         int dailyWage; 
+	      
+	         while(TOTAL_ATTENDANCE <=20 && TOTAL_HRS <=100){ 
+	             for(int day=1; day<=WORKING_DAYS; day++){ 
+	                 int empCheck = (int)(Math.random() * 10) % 2; 
+	                 if(checkIsPresent(empCheck)){ 
+	                     int empType = (int)(Math.random() * 10) % 2; 
+	                     dailyWage = calcDailyEmpWage(empType);    
+	                     monthlyWage += dailyWage; 
+	                 } 
+	             } 
+	         } 
+	         return monthlyWage; 
+	     }
 }
--- src/com/bridgelabs/empwage/EmployeeWageWelcome.java
+++ src/com/bridgelabs/empwage/EmployeeWageWelcome.java
@@ -4,9 +4,9 @@ public class EmployeeWageWelcome {
 	public static void main (String args[]) {
 		
 		System.out.println(" Welcome to Employee Wage Computation Program"); // Master Branch  
-		EmployeeWage emp = new EmployeeWage();
+		EmployeeWage emp = new EmployeeWage(20,20);
 		int wage = emp.calcMonthlyWage();
-		System.out.println("Montly Wage is :" +wage);
+		System.out.println("Employee Montly Wage is :" +wage);
 		
 	}
 }

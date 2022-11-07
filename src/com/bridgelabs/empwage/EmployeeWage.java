e9a80a2a1f1d5307f4802f81e02e86f2e2857cf7
unknown <shivam18feb@gmail.com>
2022-11-07 18:20:20 +0530

    ParttimeWagecal

--- src/com/bridgelabs/empwage/EmployeeWage.java
+++ src/com/bridgelabs/empwage/EmployeeWage.java
@@ -3,7 +3,8 @@ package com.bridgelabs.empwage;
 public class EmployeeWage {
 	int IS_PRESENT = 1;
 	int WAGE_PER_HRS = 20;
-	int FULL_DAY_HRS = 8;
+	int FULL_DAY_HRS = 16;
+	int FULL_TIME = 1;
 	
 	public boolean checkIsPresent(double empCheck) {
 		if(empCheck == IS_PRESENT) {
@@ -14,8 +15,17 @@ public class EmployeeWage {
 		}
 			
 	}
-	 public int calcDailyEmpWage() {
+	 public int calcDailyEmpWage(double empType) {
 		 int dailyEmpWage = WAGE_PER_HRS * FULL_DAY_HRS;
+		 if (empType == FULL_TIME) {
+			 System.out.println("Emp Type : Full time");
+			 System.out.println("Daily Emp Wage : "+dailyEmpWage);
+		 }
+		 else {
+			 System.out.println("Emp Type : Part Time");
+			 System.out.println("Daily Emp Wage : "+(dailyEmpWage/2));
+			 
+		 }
 		 return dailyEmpWage;
 	 }
 
--- src/com/bridgelabs/empwage/EmployeeWageWelcome.java
+++ src/com/bridgelabs/empwage/EmployeeWageWelcome.java
@@ -6,9 +6,10 @@ public class EmployeeWageWelcome {
 		System.out.println(" Welcome to Employee Wage Computation Program"); // Master Branch  
 		EmployeeWage emp = new EmployeeWage();
 		double empCheck = Math.floor(Math.random()*10) % 2;
+		double empType = Math.floor(Math.random()*10) % 2;
 		
 		if(emp.checkIsPresent(empCheck)) {
-			System.out.println("Daily Emp Wages :- "+emp.calcDailyEmpWage());
+			emp.calcDailyEmpWage(empType);
 			}
 		else {
 		System.out.println("Its Absent");
